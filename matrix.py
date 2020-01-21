c4156d6c2e41dd034e36a0ba55df02308256152a
LazavikouArtsiom <lazovikovartyom@mail.ru>
2020-01-21 17:26:59 +0300

    Matrix class foundation

---
+++ matrix.py
@@ -0,0 +1,37 @@
+class Matrix():
+    def __init__(self, n, m):
+        self.n = n
+        self.m = m
+
+    def fill(self, randomize=True, manually=False):
+        pass
+
+    def transpose(self):
+        pass
+
+    def get_row(self, row):
+        pass 
+
+    def get_col(self, col):
+        pass
+
+    def get_main_diagonal(self):
+        pass
+    
+    def is_diagonal(self, tup):
+        pass
+
+    def is_square(self):
+        pass
+
+    def shape(self):
+        pass
+
+    def __add__(self, other_matrix):
+        pass
+
+    def __sub__(self, other_matrix):
+        pass
+
+    def __mul__(self, other_matrix):
+        pass
