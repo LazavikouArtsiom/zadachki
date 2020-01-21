001a813de341b7d359d5f52219ebcc6f1b88da54
LazavikouArtsiom <lazovikovartyom@mail.ru>
2020-01-21 12:16:39 +0300

    4 changed, 8 ended

--- zadachki.py
+++ zadachki.py
@@ -15,8 +15,9 @@ def first_occurance(haystack, needle):
 
 
 def increment_last(array):
+    array = [int(el) for el in array]
     array[-1] += 1
-    return array
+    return [str(el) for el in array]
 
 
 from collections import Counter
@@ -33,10 +34,21 @@ def single_one_1(ls):
             counter[element] += 1
     return [v for k, v in counter.items() if v == 1]
 
+def reverse_string_by_words(strng):
+    return ' '.join(strng.split()[::-1])
+
+def find_major_element(array):
+    unique_el = list(set(array))
+    temp = [x for x in Counter(array).values()]
+    return unique_el[temp.index(max(temp))]
+
+
 
 if __name__ == '__main__':
-    print(find_median([1, 3, 4, 7], [2, 5, 6, 8]))
-    print(ispalindrome_int(121))
-    print(first_occurance("aoaoa", "ao"))
-    print(increment_last([1, 2, 3, 4]))
-    print(single_one_1([1, 1, 2, 2, 3]))
+    #print(find_median([1, 3, 4, 7], [2, 5, 6, 8]))
+    #print(ispalindrome_int(121))
+    #print(first_occurance("aoaoa", "ao"))
+    print(increment_last(['1', '2', '3', '4']))
+    #print(single_one_1([1, 1, 2, 2, 3]))
+    #print(reverse_string_by_words("asd asd dsa sad"))
+    #print(find_major_element([1,1,2,3,2,2,2,1,2,3]))
